c856f3b468f6c483fb60c96bf8c49f1936e8d47c
frontstall <deepspiral@yandex.ru>
2020-04-10 21:54:48 +0300

    solve 2nd task

--- src/helpers/processSequence.js
+++ src/helpers/processSequence.js
@@ -1,51 +1,113 @@
 /**
  * @file Домашка по FP ч. 2
- * 
+ *
  * Подсказки:
  * Метод get у инстанса Api – каррированый
  * GET / https://animals.tech/{id}
- * 
+ *
  * GET / https://api.tech/numbers/base
  * params:
  * – number [Int] – число
  * – from [Int] – из какой системы счисления
  * – to [Int] – в какую систему счисления
- * 
+ *
  * Иногда промисы от API будут приходить в состояние rejected, (прямо как и API в реальной жизни)
  * Ответ будет приходить в поле {result}
  */
+import * as R from 'ramda';
 import Api from '../tools/api';
 
 const api = new Api();
 
-/**
- * Я – пример, удали меня
- */
-const wait = time => new Promise(resolve => {
-    setTimeout(resolve, time);
-})
+const resolve = (value) => Promise.resolve(value);
 
-const processSequence = ({value, writeLog, handleSuccess, handleError}) => {
-    /**
-     * Я – пример, удали меня
-     */
-    writeLog(value);
+const askBinary = R.pipeWith(R.then)([
+  resolve,
+  R.set(
+    R.lensProp('number'),
+    R.__,
+    { from: 10, to: 2, number: null },
+  ),
+  R.append(R.__, ['https://api.tech/numbers/base']),
+  R.apply(api.get),
+  R.prop('result'),
+]);
 
-    api.get('https://api.tech/numbers/base', {from: 2, to: 10, number: '01011010101'}).then(({result}) => {
-        writeLog(result);
-    });
+const askAnimal = R.pipeWith(R.then)([
+  resolve,
+  String,
+  R.concat('https://animals.tech/'),
+  api.get(R.__, null),
+  R.prop('result'),
+]);
 
-    wait(2500).then(() => {
-        writeLog('SecondLog')
+const validate = R.allPass([
+  R.compose(
+    Number.isFinite,
+    Number,
+  ),
+  R.compose(
+    R.both(
+      R.gt(R.__, 1),
+      R.lt(R.__, 10),
+    ),
+    R.length,
+  ),
+  R.anyPass([
+    R.startsWith('0.'),
+    R.compose(
+      R.test(/[1-9]/),
+      R.head,
+    ),
+  ]),
+  R.compose(
+    R.gt(R.__, 0),
+    Number,
+  ),
+]);
 
-        return wait(1500);
-    }).then(() => {
-        writeLog('ThirdLog');
+const normalize = R.compose(
+  Math.round,
+  Number,
+);
 
-        return wait(400);
-    }).then(() => {
-        handleSuccess('Done');
-    });
-}
+const square = R.converge(
+  R.multiply,
+  [R.identity, R.identity],
+);
+
+const VALIDATION_ERROR_MESSAGE = 'ValidationError';
+
+const processSequence = async ({
+  value, writeLog, handleSuccess, handleError,
+}) => {
+  try {
+    await R.pipe(
+      R.tap(writeLog),
+      R.ifElse(
+        validate,
+        R.pipeWith(R.then)([
+          resolve,
+          normalize,
+          R.tap(writeLog),
+          askBinary,
+          R.tap(writeLog),
+          R.length,
+          R.tap(writeLog),
+          square,
+          R.tap(writeLog),
+          R.modulo(R.__, 3),
+          R.tap(writeLog),
+          askAnimal,
+          R.tap(writeLog),
+          handleSuccess,
+        ]),
+        () => handleError(VALIDATION_ERROR_MESSAGE),
+      ),
+    )(value);
+  } catch (e) {
+    handleError(e);
+  }
+};
 
 export default processSequence;
